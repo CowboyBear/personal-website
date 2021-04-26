4e5d1d9a10553a77c0e1bb1bc8b41c46aa835698
Matheus Jatczak Bertazzo <matheus.j.bertazzo@gmail.com>
2021-04-26 20:10:37 -0300

    Routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { CareerComponent } from './components/career/career.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutMeComponent },
+  { path: 'career', component: CareerComponent },
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { CareerComponent } from './components/career/career.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,    
+    CareerComponent, NotFoundComponent
   ],
   imports: [
     BrowserModule,
