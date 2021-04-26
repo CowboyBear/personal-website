1f4894295bac1e79c8a1e0732b04e6cf0c0dd599
Matheus Jatczak Bertazzo <matheus.j.bertazzo@gmail.com>
2021-04-26 01:19:51 -0300

    Side menu implemented; angular material and materialize added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'mb-personal-website';
+  title = 'mb-personal-website';  
+  @ViewChild("drawer") drawer: MatDrawer;
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.    
+    // this.drawer.open();
+  }
 }
